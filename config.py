4844f0afe9767ac4901f6daf77a543a337062038
Parth <desaiparth08@gmail.com>
2015-06-18 01:36:57 +0530

    Updating configuration

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 __author__ = 'parth'
 
 
-consumer_key="dU6FW8BilBjPz5q5KlmmTYsVw"
-consumer_secret="LhO98qUeUcczFNi6IrFroGYtbMr5s4eKDFNcTm15Nsz1ApkdLD"
+consumer_key="xxx"
+consumer_secret="xxx"
 
-access_token="161592693-tK0ruDcmtJHJGv9DPYfPapFqKamOZflgoaUoM6M0"
-access_token_secret="pWX8ucflOuZUc7dEibFBB3kOHI8C36UJypx2nOTsFwggR"
+access_token="xxx"
+access_token_secret="xxx"
 
-database_uri="postgresql://demo:123@localhost/tweet_stats"
+database_uri="postgresql://u_name:pass@localhost/tweet_stats"
